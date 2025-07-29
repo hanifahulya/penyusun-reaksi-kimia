afbd44b1cc37853037ae0ab0dde9a2a4b87ccb34
hanifah ulya <ulyahanifah4@gmail.com>
2025-07-30 00:41:07 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import streamlit as st
 from reaction_engine.main import susun_reaksi_dari_unsur, hitung_massa_molekul
 from periodic_table_ui.tabel import tampilkan_tabel_periodik
-from utils.tabel_periodik_118 import elemen_periodik
+from utils.tabel_periodik_118.tabel_periodik_118 import elemen_periodik
 
 st.set_page_config(page_title='Penyusun Persamaan Reaksi Kimia', layout='wide')
 
 st.sidebar.title('Navigasi')
-halaman = st.sidebar.radio('Pilih Halaman', ['Dasar Teori', 'Penyusun Reaksi'])
+halaman = st.sidebar.radio('Pilih Halaman', ['Beranda', 'Dasar Teori'])
 
 if halaman == 'Dasar Teori':
     st.title("Dasar Teori")
     st.markdown("""
-    Aplikasi ini dirancang untuk membantu dalam menyusun persamaan reaksi kimia secara otomatis.
-    Pengguna dapat memilih unsur-unsur dari tabel periodik untuk menghasilkan reaksi yang setara,
-    disertai jenis reaksinya, serta menghitung massa molekul relatif (BM) senyawa yang terbentuk.
+    Aplikasi ini digunakan untuk menyusun persamaan reaksi kimia dari unsur-unsur yang tersedia 
+    pada tabel periodik. Pengguna dapat memilih dua unsur, dan aplikasi akan mencoba menyusun 
+    persamaan reaksinya jika tersedia. Hasil reaksi akan ditampilkan dalam bentuk reaksi setara, 
+    jenis reaksi, serta massa molekul (BM) dari senyawa hasil reaksi.
     """)
-elif halaman == 'Penyusun Reaksi':
-    st.title("Penyusun Persamaan Reaksi Kimia")
-    tampilkan_tabel_periodik()
+else:
+    tampilkan_tabel_periodik(elemen_periodik)

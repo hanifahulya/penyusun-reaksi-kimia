ba3349beec44769d6eb611b2367942041a470ac8
hanifah ulya <ulyahanifah4@gmail.com>
2025-07-30 00:32:32 +0700

    Add files via upload

---
+++ utils/data.py
@@ -0,0 +1,7 @@
+elemen_periodik = [
+    {"simbol": "H", "nama": "Hidrogen", "golongan": "nonlogam"},
+    {"simbol": "Fe", "nama": "Besi", "golongan": "logam transisi"},
+    {"simbol": "Cl", "nama": "Klorin", "golongan": "halogen"},
+    {"simbol": "Pb", "nama": "Timbal", "golongan": "logam"},
+    {"simbol": "O", "nama": "Oksigen", "golongan": "nonlogam"}
+]

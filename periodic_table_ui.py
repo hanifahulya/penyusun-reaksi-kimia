98e69c3cbec0527b62084edf01d8176d8a0216cc
hanifah ulya <ulyahanifah4@gmail.com>
2025-07-30 14:56:56 +0700

    Add files via upload

--- periodic_table_ui.py
+++ periodic_table_ui.py
@@ -1,30 +1,20 @@
 import streamlit as st
-from utils.tabel_periodik_118 import elemen_periodik, warna_golongan
+from utils.tabel_periodik_118 import elemen_periodik, Ar_tiap_unsur
 
-def tampilkan_tabel_periodik(filter_golongan=None, dengan_warna=True):
-    st.markdown("<style>div.stButton > button { width: 3em; height: 3em; }</style>", unsafe_allow_html=True)
-
-    selected_elements = st.session_state.get("selected_elements", [])
-
-    grid = [[None for _ in range(18)] for _ in range(10)]
-
-    for el in elemen_periodik:
-        if filter_golongan and el['golongan'] != filter_golongan:
-            continue
-        row, col = el["row"] - 1, el["column"] - 1
-        grid[row][col] = el
-
-    for row in grid:
-        cols = st.columns(18)
-        for i, el in enumerate(row):
-            if el:
-                simbol = el["simbol"]
-                warna = warna_golongan.get(el["golongan"], "#FFFFFF") if dengan_warna else "#FFFFFF"
-                if cols[i].button(simbol, key=f"{simbol}_{i}"):
-                    if simbol not in selected_elements:
-                        selected_elements.append(simbol)
-                        if len(selected_elements) > 2:
-                            selected_elements = selected_elements[-2:]
-                    st.session_state.selected_elements = selected_elements
+def tampilkan_tabel_periodik(filter_golongan=None):
+    for baris in elemen_periodik:
+        kolom = st.columns(len(baris))
+        for i, elemen in enumerate(baris):
+            simbol = elemen.get("simbol", "")
+            golongan = elemen.get("golongan", "lainnya")
+            if simbol and (filter_golongan is None or golongan == filter_golongan):
+                Ar = Ar_tiap_unsur.get(simbol, "")
+                label = f"{simbol}"
+                tooltip = f"{simbol} (Ar = {Ar})" if Ar else simbol
+                if kolom[i].button(label, help=tooltip, key=f"{simbol}_{i}", use_container_width=True):
+                    if "selected_elements" not in st.session_state:
+                        st.session_state.selected_elements = []
+                    if len(st.session_state.selected_elements) < 2 and simbol not in st.session_state.selected_elements:
+                        st.session_state.selected_elements.append(simbol)
             else:
-                cols[i].empty()
+                kolom[i].markdown(" ")

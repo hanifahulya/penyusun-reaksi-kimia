87a0cc121c906f3e6d29e06b224a378121e52639
hanifah ulya <ulyahanifah4@gmail.com>
2025-07-30 11:01:10 +0700

    Add files via upload

---
+++ utils/tabel_periodik_118.py
@@ -0,0 +1,7 @@
+elemen_periodik = [
+    {"simbol": "H", "golongan": "Nonlogam", "posisi": 0, "warna": "#f4cccc"},
+    {"simbol": "Fe", "golongan": "Logam Transisi", "posisi": 7, "warna": "#c9daf8"},
+    {"simbol": "Cl", "golongan": "Halogen", "posisi": 16, "warna": "#d9ead3"},
+    {"simbol": "Pb", "golongan": "Logam Pasca Transisi", "posisi": 13, "warna": "#fff2cc"},
+    {"simbol": "O", "golongan": "Nonlogam", "posisi": 15, "warna": "#f4cccc"}
+]

79163a9a94de1f9fda106fdc42b2ce08934b20d7
hanifah ulya <ulyahanifah4@gmail.com>
2025-07-30 14:17:05 +0700

    Update periodic_table_ui.py

--- periodic_table_ui.py
+++ periodic_table_ui.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utils.tabel_periodik_118 import elemen_periodik, Ar_tiap_unsur
 
-def tampilkan_tabel_periodik(filter_golongan=None):
+def tampilkan_tabel_periodik(filter_golongan=None, dengan_warna=False):
     for baris in elemen_periodik:
         kolom = st.columns(len(baris))
         for i, elemen in enumerate(baris):

2cee78f4540aeb7dc0bed935b55127a8b21a1b33
hanifah ulya <ulyahanifah4@gmail.com>
2025-07-30 17:33:44 +0700

    Add files via upload

---
+++ element_info.py
@@ -0,0 +1,710 @@
+element_info = {
+    "H": {
+        "nama": "Hidrogen",
+        "golongan": "nonlogam",
+        "status": "nonlogam",
+        "kegunaan": "Bahan bakar dan produksi amonia"
+    },
+    "He": {
+        "nama": "Helium",
+        "golongan": "gas mulia",
+        "status": "nonlogam",
+        "kegunaan": "Balon, pendingin, MRI"
+    },
+    "Li": {
+        "nama": "Litium",
+        "golongan": "logam alkali",
+        "status": "logam",
+        "kegunaan": "Baterai litium-ion"
+    },
+    "Be": {
+        "nama": "Berilium",
+        "golongan": "logam alkali tanah",
+        "status": "logam",
+        "kegunaan": "Industri pesawat dan nuklir"
+    },
+    "B": {
+        "nama": "Boron",
+        "golongan": "metaloid",
+        "status": "metaloid",
+        "kegunaan": "Sabun, fiberglass"
+    },
+    "C": {
+        "nama": "Karbon",
+        "golongan": "nonlogam",
+        "status": "nonlogam",
+        "kegunaan": "Bahan dasar makhluk hidup, bahan bakar"
+    },
+    "N": {
+        "nama": "Nitrogen",
+        "golongan": "nonlogam",
+        "status": "nonlogam",
+        "kegunaan": "Pupuk, refrigeran"
+    },
+    "O": {
+        "nama": "Oksigen",
+        "golongan": "nonlogam",
+        "status": "nonlogam",
+        "kegunaan": "Respirasi dan pembakaran"
+    },
+    "F": {
+        "nama": "Fluorin",
+        "golongan": "halogen",
+        "status": "nonlogam",
+        "kegunaan": "Pasta gigi, refrigeran"
+    },
+    "Ne": {
+        "nama": "Neon",
+        "golongan": "gas mulia",
+        "status": "nonlogam",
+        "kegunaan": "Lampu neon"
+    },
+    "Na": {
+        "nama": "Natrium",
+        "golongan": "logam alkali",
+        "status": "logam",
+        "kegunaan": "Garam dapur, lampu jalan"
+    },
+    "Mg": {
+        "nama": "Magnesium",
+        "golongan": "logam alkali tanah",
+        "status": "logam",
+        "kegunaan": "Kembang api, logam ringan"
+    },
+    "Al": {
+        "nama": "Aluminium",
+        "golongan": "logam pasca-transisi",
+        "status": "logam",
+        "kegunaan": "Kaleng, pesawat"
+    },
+    "Si": {
+        "nama": "Silikon",
+        "golongan": "metaloid",
+        "status": "metaloid",
+        "kegunaan": "Semikonduktor, kaca"
+    },
+    "P": {
+        "nama": "Fosfor",
+        "golongan": "nonlogam",
+        "status": "nonlogam",
+        "kegunaan": "Pupuk, deterjen"
+    },
+    "S": {
+        "nama": "Sulfur",
+        "golongan": "nonlogam",
+        "status": "nonlogam",
+        "kegunaan": "Pupuk, bahan peledak"
+    },
+    "Cl": {
+        "nama": "Klorin",
+        "golongan": "halogen",
+        "status": "nonlogam",
+        "kegunaan": "Disinfektan, plastik PVC"
+    },
+    "Ar": {
+        "nama": "Argon",
+        "golongan": "gas mulia",
+        "status": "nonlogam",
+        "kegunaan": "Lampu pijar, pengelasan"
+    },
+    "K": {
+        "nama": "Kalium",
+        "golongan": "logam alkali",
+        "status": "logam",
+        "kegunaan": "Pupuk, nutrisi tanaman"
+    },
+    "Ca": {
+        "nama": "Kalsium",
+        "golongan": "logam alkali tanah",
+        "status": "logam",
+        "kegunaan": "Tulang, semen"
+    },
+    "Sc": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ti": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "V": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Cr": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Mn": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Fe": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Co": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ni": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Cu": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Zn": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ga": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ge": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "As": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Se": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Br": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Kr": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Rb": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Sr": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Y": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Zr": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Nb": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Mo": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Tc": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ru": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Rh": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Pd": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ag": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Cd": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "In": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Sn": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Sb": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Te": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "I": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Xe": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Cs": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ba": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "La": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ce": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Pr": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Nd": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Pm": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Sm": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Eu": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Gd": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Tb": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Dy": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ho": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Er": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Tm": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Yb": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Lu": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Hf": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ta": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "W": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Re": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Os": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ir": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Pt": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Au": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Hg": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Tl": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Pb": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Bi": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Po": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "At": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Rn": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Fr": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ra": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ac": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Th": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Pa": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "U": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Np": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Pu": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Am": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Cm": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Bk": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Cf": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Es": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Fm": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Md": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "No": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Lr": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Rf": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Db": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Sg": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Bh": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Hs": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Mt": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ds": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Rg": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Cn": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Nh": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Fl": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Mc": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Lv": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Ts": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    },
+    "Og": {
+        "nama": "-",
+        "golongan": "-",
+        "status": "-",
+        "kegunaan": "-"
+    }
+}
